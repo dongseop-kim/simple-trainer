747dbedd63e1b64e0e9db8ddda8d80e0a9ce25ce
dongseopkim <dongseop.kim@vuno.co>
2024-10-31 13:47:27 +0900

    add label smoothing

--- strainer/criteria/ce.py
+++ strainer/criteria/ce.py
@@ -16,14 +16,21 @@ class CrossEntropy(nn.CrossEntropyLoss):
 
 
 class BinaryCrossEntropy(nn.BCEWithLogitsLoss):
-    def __init__(self, reduction: str = 'mean'):
+    def __init__(self, reduction: str = 'mean', label_smoothing: float = 0.0):
         super().__init__()
         self.criteria = nn.BCEWithLogitsLoss(reduction=reduction)
+        self.label_smoothing = label_smoothing
 
     def forward(self, logit: T, target: T) -> dict[str, T]:
         if target.shape != logit.shape:
             target = target.view(logit.shape)
         logit = logit.float()
         target = target.float()
+        
+        # Label smoothing when label_smoothing > 0
+        if self.label_smoothing > 0:
+            target = target * (1 - self.label_smoothing) + self.label_smoothing / 2
+        
         loss: T = self.criteria(logit, target)
+
         return {'loss': loss}
